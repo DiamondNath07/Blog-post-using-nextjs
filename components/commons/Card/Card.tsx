4eac07a6448c1250fe9f785be8b0ed4e2f0bcf42
Nathaniel <diamondnathaniel2@gmail.com>
2024-01-27 12:09:39 +0100

    add styles for cards

--- components/commons/Card/Card.tsx
+++ components/commons/Card/Card.tsx
@@ -4,16 +4,27 @@ import { CardProps } from "./card.interface";
 
 const Card: React.FC<CardProps> = ({ blog }) => {
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-      <div className="bg-[#F4F4F4] w-full flex justify-center align-middle">
-        <img className="rounded-t-lg w-64 h-64" src={blog.image} alt="" />
+    <div
+      className="max-w-xs m-auto bg-white rounded-lg shadow flex flex-col  "
+      style={{
+        height: "516px",
+      }}
+    >
+      <div className="bg-[#F4F4F4]  flex justify-center   ">
+        <img
+          className="rounded-t-lg w-full h-60 object-contain	 "
+          src={blog.image}
+          alt=""
+        />
       </div>
 
-      <div className="p-5">
-        <p className="text-sm text-gray-600 font-bold">
-          {blog.tags.join(" . ")}
+      <div className="mt-8 max-w-72	mx-auto flex flex-col justify-center items-center px-4">
+        <p className="text-sm font-medium" style={{
+          color:"#667085"
+        }}>
+          {blog.tags.join(" . ")} &#x2022;
         </p>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-2xl mt-5">
           {blog.title}
         </p>
         <p>
